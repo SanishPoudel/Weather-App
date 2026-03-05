8e8f378262bbf8fd1e1ce4805804add2c7b02945
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-03-05 17:45:57 +1100

    strigified the json

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const main = document.querySelector(".main");
 
 
 async function getData(city){
-    let city = "Melbourne";
+    // let city = "Melbourne";
     let temp_unit = "metric"
     const str = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "?unitGroup=" + temp_unit + "&key=GDGU3H48VVK9DYU3DKVF5V3UK&contentType=json";
     const response = await fetch(str,{mode:"cors"});
     const value = await response.json()
-    console.log(value["days"]);
-    main.textContent = value["days"];
+    // console.log(value["days"]);
+    main.textContent = JSON.stringify(value["days"]);
 }
 
 const btn = document.querySelector("button");
