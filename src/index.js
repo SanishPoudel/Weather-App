1ec78f7e29e681039cdeccb64bdb1c62f7598bdf
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-03-08 17:13:50 +1100

    provides correct parameters for functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { displayToday } from "./dom.js"
+import { displayToday } from "./displayToday.js"
+import { otherDays } from "./otherDays.js";
 
 const main = document.querySelector(".main");
 const btn = document.querySelector("button");
@@ -11,13 +12,19 @@ async function getData(city) {
     const str = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "?unitGroup=" + temp_unit + "&key=GDGU3H48VVK9DYU3DKVF5V3UK&contentType=json";
     const response = await fetch(str,{mode:"cors"});
     const value = await response.json()
-    // console.log(value["days"]);
+
     let weatherData = value["days"];
     let todayData = weatherData[0];
-
+    
+    let thisWeek = [];
+    for (let i = 0; i<7; i++) {
+        thisWeek[i] = weatherData[i];
+    }
+    console.log(thisWeek);
+    
     displayToday(todayData);
+    otherDays(thisWeek);
 
-    main.textContent = JSON.stringify(value["days"]);
 }
 
 
