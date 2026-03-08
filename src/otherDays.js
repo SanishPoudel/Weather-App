4ebbd0b866bffd496d5f5645af30b9e64918b737
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-03-08 17:44:55 +1100

    removed the getWeekday function

--- src/otherDays.js
+++ src/otherDays.js
@@ -1,15 +1,11 @@
+import { getWeekday } from "./getWeekday.js";
+
 function otherDays(thisWeek) {
     // function to display the rest of the days of the week in the dom. 
     thisWeek.forEach(day => {
         let weekDay = getWeekday(new Date(day["datetime"]));
         console.log(weekDay + " : " + day["temp"]);
     });
-}
-
-function getWeekday(date) {
-    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = weekDays[date.getDay()];
-    return day;
 }
 
 export { otherDays }
