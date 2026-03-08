8966509eefcedeefc9f0de598dc35e039f945966
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-03-08 17:14:22 +1100

    displays weekly temperature in console

--- src/otherDays.js
+++ src/otherDays.js
@@ -1,5 +1,15 @@
-function otherDays() {
+function otherDays(thisWeek) {
     // function to display the rest of the days of the week in the dom. 
+    thisWeek.forEach(day => {
+        let weekDay = getWeekday(new Date(day["datetime"]));
+        console.log(weekDay + " : " + day["temp"]);
+    });
+}
+
+function getWeekday(date) {
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = weekDays[date.getDay()];
+    return day;
 }
 
 export { otherDays }
