949c71580800c09f88227e0e5e6fe16c3d15c66b
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-03-08 17:56:31 +1100

    now shows data in dom rather than console

--- src/otherDays.js
+++ src/otherDays.js
@@ -2,9 +2,22 @@ import { getWeekday } from "./getWeekday.js";
 
 function otherDays(thisWeek) {
     // function to display the rest of the days of the week in the dom. 
+
+    //query selector
+    const div = document.querySelector(".other");
+
     thisWeek.forEach(day => {
         let weekDay = getWeekday(new Date(day["datetime"]));
-        console.log(weekDay + " : " + day["temp"]);
+        let value = weekDay + " : " + day["temp"];
+
+        // creating child node
+        const valueSection = document.createElement("div");
+        
+        // assigning value to child node
+        valueSection.textContent = value;
+
+        // appending child node to parent
+        div.appendChild(valueSection);
     });
 }
 
