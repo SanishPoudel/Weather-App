966c80194d3216ecd0d876649b8ccdab03adc247
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-04-24 00:10:13 +1000

    imported all svgs as variables

--- src/getIcon.js
+++ src/getIcon.js
@@ -1,2 +1,12 @@
 // import imgages as variables
 // define a function that returns variables matching the STATUS parameter
+
+import clearDay from "../img/clear-day.svg"
+import clearNight from "../img/clear-night.svg"
+import cloudy from "../img/cloudy.svg"
+import fog from "../img/fog.svg"
+import partlyCloudyDay from "../img/partly-cloudy-day.svg"
+import partlyCloudyNight from "../img/partly-cloudy-night.svg"
+import rain from "../img/rain.svg"
+import snow from "../img/snow.svg"
+import wind from "../img/wind.svg"
