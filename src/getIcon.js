815f38a6f5be4617f51d4d237f021412c1379416
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-04-26 21:18:31 +1000

    fixed typo

--- src/getIcon.js
+++ src/getIcon.js
@@ -1,27 +1,27 @@
 // import imgages as variables
 // define a function that returns variables matching the STATUS parameter
 
-import clearDay from "../img/clear-day.svg";
-import clearNight from "../img/clear-night.svg";
-import cloudy from "../img/cloudy.svg";
-import fog from "../img/fog.svg";
-import partlyCloudyDay from "../img/partly-cloudy-day.svg";
-import partlyCloudyNight from "../img/partly-cloudy-night.svg";
-import rain from "../img/rain.svg";
-import snow from "../img/snow.svg";
-import wind from "../img/wind.svg";
+import clearDay from "./img/clear-day.svg";
+import clearNight from "./img/clear-night.svg";
+import cloudy from "./img/cloudy.svg";
+import fog from "./img/fog.svg";
+import partlyCloudyDay from "./img/partly-cloudy-day.svg";
+import partlyCloudyNight from "./img/partly-cloudy-night.svg";
+import rain from "./img/rain.svg";
+import snow from "./img/snow.svg";
+import wind from "./img/wind.svg";
 
 function getIcon(status) {
     if (status === "clear-day.svg") {
         return clearDay;
     }
-    else if (staus === "clear-night.svg") {
+    else if (status === "clear-night.svg") {
         return clearNight;
     }
-    else if (staus === "cloudy.svg") {
+    else if (status === "cloudy.svg") {
         return cloudy;
     }
-    else if (staus === "fog.svg") {
+    else if (status === "fog.svg") {
         return fog;
     }
     else if (status === "partly-cloudy-day.svg") {
